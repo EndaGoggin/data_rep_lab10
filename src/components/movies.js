c49826eb24235d8e867ffba9fa17aebce959684e
Enda Goggin <g00369903@gmit.ie>
2020-10-27 13:39:49 +0000

    Lab complete

---
+++ src/components/movies.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { MovieItem } from './movieItem';
+
+export class Movies extends React.Component{
+
+    // Pass individual movies into movieItem component
+    render(){
+        return this.props.movies.map( (movie)=>{
+            return <MovieItem movie={movie}></MovieItem>
+        })
+    }
+}
